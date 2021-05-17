8c8ce2528f1e5cf023b61895a2d71e7b43f03218
Przem1998 <p.zeleznicki@protonmail.com>
2021-05-17 21:51:31 +0200

    init http client module

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import {IProduct} from './models/product';
+
+import {IPagination} from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'PlumberWebShop';
+export class AppComponent implements OnInit {
+  title = 'WebShop';
+  products:any[];
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products').subscribe((respone: IPagination)=> {
+        this.products=respone.data
+    },error =>{
+      console.log(error);
+    }
+    );
+  }
+
 }
