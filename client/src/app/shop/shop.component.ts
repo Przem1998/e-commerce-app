2b7d8cf819b1e7775326fb3ef372504a0bdffc0d
Przem1998 <p.zeleznicki@protonmail.com>
2021-05-29 18:19:41 +0200

    move pagination elements: pages and report pages to saparately components

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -67,10 +67,7 @@ export class ShopComponent implements OnInit {
     this.getProducts();
   }
   onPageChanged(event:any){
-    this.shopParams.pageNumber=event.page;
+    this.shopParams.pageNumber=event;
     this.getProducts();
-  }
-  onItemsResults(event:any){
-    
   }
 }
