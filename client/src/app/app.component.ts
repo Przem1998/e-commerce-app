235d6d439084b495af5034a5cb76e3ed272c1c9b
Przem1998 <p.zeleznicki@protonmail.com>
2021-05-29 13:00:50 +0200

    Refactoring httpClient and binding product, brand and type selectors

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
-import {IProduct} from './models/product';
-
-import {IPagination} from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -12,16 +8,10 @@ import {IPagination} from './models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'WebShop';
-  products:any[];
-  constructor(private http: HttpClient){}
 
+constructor(){}
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products').subscribe((respone: IPagination)=> {
-        this.products=respone.data
-    },error =>{
-      console.log(error);
-    }
-    );
+
   }
 
 }
