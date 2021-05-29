447bed68416ebb52445ebe5c3e2fc4e6645174cf
Przem1998 <p.zeleznicki@protonmail.com>
2021-05-29 18:59:50 +0200

    fix bug during filtering and prevent override request

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { IBrand } from '../shared/models/brand';
 import { IProduct } from '../shared/models/product';
 import { IType } from '../shared/models/productType';
@@ -11,6 +11,8 @@ import { ShopService } from './shop.service';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
+  @ViewChild("search", {static: true}) searchTerm: ElementRef;
+
   products: IProduct[];
   brands: IBrand[];
   types: IType[];
@@ -56,10 +58,12 @@ export class ShopComponent implements OnInit {
   }
   onBrandSelected(brandId: number){
     this.shopParams.brandId=brandId;
+    this.shopParams.pageNumber=1
     this.getProducts();
   }
   onTypeSelected(typeId: number){
     this.shopParams.typeId=typeId;
+    this.shopParams.pageNumber=1
     this.getProducts();
   }
   onSortSelected(sort: string){
@@ -67,7 +71,19 @@ export class ShopComponent implements OnInit {
     this.getProducts();
   }
   onPageChanged(event:any){
-    this.shopParams.pageNumber=event;
+    if(this.shopParams.pageNumber !== event){
+      this.shopParams.pageNumber=event;
+      this.getProducts();
+    }
+  }
+  onSearch(){
+    this.shopParams.search= this.searchTerm.nativeElement.value;
+    this.shopParams.pageNumber=1
+    this.getProducts();
+  }
+  onReset(){
+    this.searchTerm.nativeElement.value="";
+    this.shopParams= new ShopParams();
     this.getProducts();
   }
 }
