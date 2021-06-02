432ed746c2d2e6e7b89088d2e2b9126573471c8c
Przem1998 <p.zeleznicki@protonmail.com>
2021-06-02 12:38:17 +0200

    init basket config

--- API/Startup.cs
+++ API/Startup.cs
@@ -7,6 +7,7 @@ using Infrastructure.Data;
 using API.Helpers;
 using API.Middleware;
 using API.Extansions;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -25,6 +26,10 @@ namespace API
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x=> x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            services.AddSingleton<ConnectionMultiplexer>(c=>{
+                var configuration=ConfigurationOptions.Parse(_config.GetConnectionString("Redis"),true);
+                return ConnectionMultiplexer.Connect(configuration);
+            });
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
             services.AddCors(opt=>{
