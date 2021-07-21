b17856f779605fa96c5b8bdc780133b6fac9288e
Przem1998 <p.zeleznicki@protonmail.com>
2021-07-21 17:26:05 +0200

    implement basket repository

--- API/Extansions/ApplicationServicesExtansions.cs
+++ API/Extansions/ApplicationServicesExtansions.cs
@@ -12,6 +12,7 @@ namespace API.Extansions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services) //extending IServiceCollectoon
         {
             services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IBasketRepository,BasketRepository>();
             services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
           
             services.Configure<ApiBehaviorOptions>(options=>
