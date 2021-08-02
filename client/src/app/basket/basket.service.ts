238fe3ccb21c2f51cfd06ee8d7e81dec93a0649a
Przem1998 <p.zeleznicki@protonmail.com>
2021-08-02 05:21:58 +0200

    persisting the basket on startup

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -19,6 +19,7 @@ export class BasketService {
     return this.http.get(this.baseUrl + 'basket?id=' + id ).pipe(
       map((basket: IBasket) => {
         this.basketSource.next(basket);
+        console.log(this.getCurrentBasketValue());
       })
     )
   }
