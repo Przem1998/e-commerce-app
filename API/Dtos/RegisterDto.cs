0c87ed3e20671f32964a60680364acde3b184eaf
Przem1998 <p.zeleznicki@protonmail.com>
2021-08-26 15:55:35 +0200

    Added annorations to address dtos, register dtos and defined security password by regex

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression(@"(?=^.{8,16}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$",
+        ErrorMessage = "Password must have 1 uppercase, 1 lowercase, 1 number and 1 non alphanumeric and 8 beetween 16 characters")]
         public string Password { get; set; }
         
     }
