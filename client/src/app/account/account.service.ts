9b129e744f166262fbaf76de0a40feef3e5fe6b5
Przem1998 <p.zeleznicki@protonmail.com>
2021-09-21 20:52:28 +0200

    inited register form with async validator

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -56,6 +56,6 @@ export class AccountService {
     this.router.navigateByUrl('/');
   }
   checkIfEmailExists(email: string){
-    return this.http.get(this.baseUrl+'/account/emailexists?email=' + email);
+    return this.http.get(this.baseUrl+'account/emailexists?email=' + email);
   }
 }
--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AsyncValidatorFn, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { of, timer } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 import { AccountService } from '../account.service';
 
 @Component({
@@ -10,6 +12,7 @@ import { AccountService } from '../account.service';
 })
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
+  errors: string[];
 
   constructor(private fb: FormBuilder, private accountService: AccountService, private router: Router) { }
 
@@ -19,7 +22,10 @@ export class RegisterComponent implements OnInit {
   createRegisterForm(){
     this.registerForm = this.fb.group({
       displayName: [null, [Validators.required]],
-      email: [null, [Validators.required, Validators.pattern("^([0-9a-zA-Z]([-.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$")]],
+      email: [null, 
+             [Validators.required, Validators.pattern("^([0-9a-zA-Z]([-.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$")],
+             [this.validateEmailNotTaken()]
+            ],
       password: [null, [Validators.required, Validators.pattern("(?=^.{8,16}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$")]]
     })
   }
@@ -28,6 +34,24 @@ export class RegisterComponent implements OnInit {
       this.router.navigateByUrl('/shop');
     }, error => {
       console.log(error);
+      this.errors= error.errors;
+      
     });
   }
+  validateEmailNotTaken() : AsyncValidatorFn {
+    return control => {
+      return timer(500).pipe(
+        switchMap(() => {
+          if(!control.value){
+            return of(null);
+          }
+          return this.accountService.checkIfEmailExists(control.value).pipe(
+            map(res => {
+              return res ? {emailExists: true} : null;
+            })
+          );
+        })
+      );
+    }
+  };
 }
