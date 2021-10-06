2b0f3345bb938d99ac4e122737cce9775eead528
Przem1998 <p.zeleznicki@protonmail.com>
2021-10-06 12:21:11 +0200

    implemented the order get methods, enpoints and dtos for order and mapper

--- API/Controllers/OrderController.cs
+++ API/Controllers/OrderController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -31,6 +32,26 @@ namespace API.Controllers
 
             if(order == null) return BadRequest(new ApiResponse(400, "Probem creating order"));
             return Ok(order);
+        }
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<OrderDto>>> GetOrdersForUser()
+        {
+            var email = HttpContext.User.RetrieveEmailFromPrincipal();
+            var orders = await _orderService.GetOrdersForUserAsync(email);
+            return Ok(_mapper.Map<IReadOnlyList<Order>, IReadOnlyList<OrderToReturnDto>>(orders));
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderToReturnDto>> GetOrderByIdUser(int id)
+        {
+            var email = HttpContext.User.RetrieveEmailFromPrincipal();
+            var order = await _orderService.GetOrderByIdAsync(id, email);
+            if(order == null) return NotFound(new ApiResponse(404));
+            return _mapper.Map<Order, OrderToReturnDto>(order);
+        }
+        [HttpGet("deliveryMethods")]
+        public async Task<ActionResult<IReadOnlyList<DeliveryMethod>>> GetDeliveryMethods()
+        {
+            return Ok(await _orderService.GetDeliveryMethodsAsync());
         }
     }
 }
--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -2,6 +2,7 @@ using API.Dtos;
 using AutoMapper;
 using Core.Entities;
 using Core.Entities.Identity;
+using Core.Entities.OrderAggregate;
 
 namespace API.Helpers
 {
@@ -13,10 +14,18 @@ namespace API.Helpers
                 .ForMember(d=>d.ProductSize,o=>o.MapFrom(s=>s.ProductSize.Size))
                 .ForMember(d=>d.ProductType,o=>o.MapFrom(s=>s.ProductType.Name))
                 .ForMember(d =>d.PictureUrl,o=>o.MapFrom<ProductURLResolver>());
-            CreateMap<Address, AddressDto>().ReverseMap();
+            CreateMap<Core.Entities.Identity.Address, AddressDto>().ReverseMap();
             CreateMap<CustomerBasketDto,CustomerBasket>();
             CreateMap<BasketItemDto,BasketItem>();
             CreateMap<AddressDto, Core.Entities.OrderAggregate.Address>();
+            CreateMap<Order, OrderToReturnDto>()
+                    .ForMember(d => d.DeliveryMethod, o => o.MapFrom(s => s.DeliveryMethod.ShortName))
+                    .ForMember(d => d.ShippingPrice, o => o.MapFrom(s => s.DeliveryMethod.Price));
+            CreateMap<OrderItem, OrderItemDto>()
+                    .ForMember(d => d.ProductId, o => o.MapFrom(s => s.ItemOrdered.Id))
+                    .ForMember(d => d.ProductName, o => o.MapFrom(s => s.ItemOrdered.Name))
+                    .ForMember(d => d.PictureUrl, o => o.MapFrom(s => s.ItemOrdered.PictureUrl))
+                    .ForMember(d => d.PictureUrl, o=> o.MapFrom<OrderItemResolver>());
         }
     }
 }
