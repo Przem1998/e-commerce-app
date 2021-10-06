2cb6e49261180662c973c89cbc35503e0fe9f2b8
Przem1998 <p.zeleznicki@protonmail.com>
2021-10-07 00:58:28 +0200

    created checkout components and formated address and delivery form

---
+++ client/src/app/checkout/checkout-succeess/checkout-succeess.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-succeess',
+  templateUrl: './checkout-succeess.component.html',
+  styleUrls: ['./checkout-succeess.component.scss']
+})
+export class CheckoutSucceessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- client/src/app/checkout/checkout.module.ts
+++ client/src/app/checkout/checkout.module.ts
@@ -3,12 +3,22 @@ import { CommonModule } from '@angular/common';
 import { CheckoutComponent } from './checkout.component';
 import { CheckoutRoutingModule } from './checkout-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { CheckoutAddressComponent } from './checkout-address/checkout-address.component';
+import { CheckoutDeliveryComponent } from './checkout-delivery/checkout-delivery.component';
+import { CheckoutReviewComponent } from './checkout-review/checkout-review.component';
+import { CheckoutPaymentComponent } from './checkout-payment/checkout-payment.component';
+import { CheckoutSucceessComponent } from './checkout-succeess/checkout-succeess.component';
 
 
 
 @NgModule({
   declarations: [
-    CheckoutComponent
+    CheckoutComponent,
+    CheckoutAddressComponent,
+    CheckoutDeliveryComponent,
+    CheckoutReviewComponent,
+    CheckoutPaymentComponent,
+    CheckoutSucceessComponent
   ],
   imports: [
     CommonModule,
